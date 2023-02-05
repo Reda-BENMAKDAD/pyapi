6bead74627aac4e2e963f504f06735acbd7451e0
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-02-06 00:00:10 +0100

    corrected indentation bug

--- HTTP/request_parser.py
+++ HTTP/request_parser.py
@@ -22,43 +22,43 @@ class RequestParser:
         
     
     def parse(self, request: str) -> None:
-    """
-    Note: this parsing of the request can go wrong in many ways. it is not fully compliant with the HTTP specifications, 
-    but should work for most of the cases.
-    if you are comfortable with the HTTP protocol, please do not hesitate to improve this function to meet the specs, thanks!    
-    """    
-    # getting the different parts of the HTTP request: request line, headers, and the body
-    request_headers_body = request.split("\r\n\r\n", 1)
-    request_and_headers = request_headers_body[0]
-    body_str: str = request_headers_body[1] # body as string from the request
-    request_line, headers = request_and_headers.split("\r\n", 1) # request line
-    headers_lstr = headers.split("\r\n") # headers as a list of strings
-    
-    
-    # parsing the first line of the request, that contains the method, the uri (ressource requested), uri params and the protocol
-    # TODO: handle errors if the request is malformed (not enough arguments for unpacking etc.)
-    method, uri, protocol = request_line.split(" ")
-    uri_params: dict = self.parse_url_params(uri)
-    
-    # parsing the headers part to get a dictionary of headers
-    headrs: dict = {}
-    for header: str in headers_lstr:
-        header_parts = header.split(": ")
-        headers[header_parts[0]] = header_parts[1]
-
-    # parsing the cookies in the headers
-    cookies: dict = {}
-    if "Cookie" in headers_obj:
-        cookies = self.parse_cookies(headers_obj["Cookie"])
-    
-    # parsing the body with the body parser only if the method is not GET
-    # TODO: find a good way to parse the body (improve the body parser to take into account the headers) and fit all of the
-    # information in the request object
-    body: dict = {}
-    if method != "GET":
-        body = self.bodyParser.parse(body_str, headers)
+        """
+        Note: this parsing of the request can go wrong in many ways. it is not fully compliant with the HTTP specifications, 
+        but should work for most of the cases.
+        if you are comfortable with the HTTP protocol, please do not hesitate to improve this function to meet the specs, thanks!    
+        """    
+        # getting the different parts of the HTTP request: request line, headers, and the body
+        request_headers_body = request.split("\r\n\r\n", 1)
+        request_and_headers = request_headers_body[0]
+        body_str: str = request_headers_body[1] # body as string from the request
+        request_line, headers = request_and_headers.split("\r\n", 1) # request line
+        headers_lstr = headers.split("\r\n") # headers as a list of strings
         
-    return Request(method, uri, protocol, uri_params, headers, cookies, body)
+        
+        # parsing the first line of the request, that contains the method, the uri (ressource requested), uri params and the protocol
+        # TODO: handle errors if the request is malformed (not enough arguments for unpacking etc.)
+        method, uri, protocol = request_line.split(" ")
+        uri_params: dict = self.parse_url_params(uri)
+        
+        # parsing the headers part to get a dictionary of headers
+        headrs: dict = {}
+        for header in headers_lstr:
+            header_parts = header.split(": ")
+            headers[header_parts[0]] = header_parts[1]
+
+        # parsing the cookies in the headers
+        cookies: dict = {}
+        if "Cookie" in headers_obj:
+            cookies = self.parse_cookies(headers_obj["Cookie"])
+        
+        # parsing the body with the body parser only if the method is not GET
+        # TODO: find a good way to parse the body (improve the body parser to take into account the headers) and fit all of the
+        # information in the request object
+        body: dict = {}
+        if method != "GET":
+            body = self.bodyParser.parse(body_str, headers)
+            
+        return Request(method, uri, protocol, uri_params, headers, cookies, body)
         
 def parse_cookies(cookies_str: str) -> dict:
     cookies_str = parse.unquote(cookies_str)
