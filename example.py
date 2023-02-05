53b301e75fbdcd6fa0c4e680314390301a056bd4
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-02-05 21:21:44 +0100

    improved explanation in comments

--- example.py
+++ example.py
@@ -1,34 +1,27 @@
+# note: this code is heavily commented to help you understand what's going on, don't get afraid lol
 from server.server import Server
 
-# we create our api by instantiating the Server class
+# we first create our api by instantiating the Server class
 api = Server(port=8000, verbose=True)
+# now we define our route handlers
 
-# defining route handlers
+# this is the function that handles the "/" route
 def root_route(req, res):
-    # this is a simple route handler that takes in:
-    # [+] a "req" parameter: it's simply a class that groupes all the information that you received from the client
-    #                         including the HTTP method used, uri, protocol, headers, cookies, body, body parameters...etc
-    #                         you can print the request object to see all the information
-    #
-    # [+] a "res" parameter: this is what you will use to send your response to the client, you can send plain text, html, xml...
-    #                        with the res.send method, or send an entire file with the res.sendfile method or res.json to send json data 
-    
-    # i will later implement methods in the res class to send images...etc
-    res.send("<!DOCTYPE html><html><head></head><body>Hello World,  go to <a href=\"/books\">books route</a></body><html>")
-    
+    # please look at the README.md file to understand these req and res objects
+    res.sendFile("./static/index.html")
 
-def foo_route(req, res):
-    res.send("Foo route")
-def query_route(req, res):
-    res.json(req.query)
-def books_route(req, res):
-    # this is a route that returns a form to enter a book name, and post it to "/books" route
-    res.send("<!DOCTYPE html><html><head></head><body>Hello World, welcome to books route <form method=\"POST\" action=\"/books\"><label>enter book name</label><input type=\"text\" name=\"bookname\" /> <input type=\"submit\"/></form></body><html>")
+# and we map our route handler to the route
+api.get("/", root_route)
 
+# at this point we can already launch the app and test it
+# api.start()
+# but we will define some other routes for the sake of example
+def book_route(req, res):
+    # this route returns a form to the client, to submit a book name
+    res.sendFile("./static/books.html")
 
 def books_post_route(req, res):
-    # this is the function that handles the POST to the "/books" route
-    # retrieving the submitted book name from the request:
+    # this function handles the form submission from the "/book" route
     book_name = req.body["data"]["bookname"]
     response = f"""<!DOCTYPE html>
 <html>
@@ -40,13 +33,10 @@ you entered {book_name} as book name
 
     res.send(response)
     
-
-# mapping route handlers to routes
-api.get("/", root_route)
-api.get("/foo", foo_route)
-api.get("/books", books_route)
-api.get("/query", query_route)
+# now we map our route handlers to the routes
+api.get("/book", books_route)
 api.post("/books", books_post_route)
 
-# launching the app
+# and we launch the app
 api.start()
+
